8be2c4b2f76ef041f2dc66f7c9ed622f927bd700
gumbomasta <gregheff@gmail.com>
2019-09-15 22:30:15 -0400

    Comitting example physics lesson!

---
+++ Assets/Lesson 3 - Physics/SimplePhysicsPush.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePhysicsPush : MonoBehaviour
+{
+
+    public float forcePower = 10f;
+    Rigidbody thisRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody>();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            thisRigidbody.AddForce(Vector3.forward * forcePower);
+        }
+    }
+}
