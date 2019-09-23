306161514105b821b2721abac84e80e0c74f6c3e
gregheff@gmail.com <gregheff@gmail.com>
2019-09-23 09:22:24 -0400

    merging for class 4

---
+++ Assets/Lesson 3 - Physics/CameraEnabler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnabler : MonoBehaviour
+{
+    public GameObject myCamera;
+
+    void OnTriggerEnter()
+    {
+        print("SOMETHING ENTERED!");
+        myCamera.SetActive(true);
+    }
+}
