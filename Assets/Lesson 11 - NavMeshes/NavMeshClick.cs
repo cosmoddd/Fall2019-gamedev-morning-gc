12f87dff38cdf5fa81d1401c62bb7ee8ce063f7b
gumbomasta <gregheff@gmail.com>
2019-11-11 00:51:30 -0500

    NavMeshes

---
+++ Assets/Lesson 11 - NavMeshes/NavMeshClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshClick : MonoBehaviour
+{
+
+    NavMeshAgent thisAgent;
+    public GameObject navigationPointer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit myHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out myHit, 1000f))
+            {
+                thisAgent.destination = myHit.point;
+
+                // set position of helper plus offset
+                navigationPointer.transform.position = (myHit.point + new Vector3(0,.1f,0));
+            }
+        }
+    }
+}
